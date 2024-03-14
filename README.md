d007fb963aa9b0ff7bab0f08c4b6e6d548fdfa51
duongkstn <nguyenduongyht@gmail.com>
2024-03-14 09:05:23 +0700

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+This repos is for learning trie algorithm
---
+++ common_multiple_strings/example.py
@@ -0,0 +1,10 @@
+from commonstrings import PyCommon_multiple_strings
+tree = PyCommon_multiple_strings()  # init
+list_str = [
+       'abc',
+       'abcxa',
+       'xamnb',
+       'yamnc',
+       'abcd'
+    ]
+tree.from_strings(list_str)
